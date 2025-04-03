84f8e2c1ad66fb9961c05fe7d5fd3c28951a73bd
yeimi-g <yeimi.guido@est.ulsa.edu.ni>
2025-04-03 13:53:52 -0600

    sonido

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 var estadolicuadora ="apagada";
+var sonidoLicuadora = document.getElementById("blender-sound");
+var botonLicuadora = document.getElementById("blender-button-sound")
 var licuadora = document.getElementById("blender");
 
 function controlarLicuadora(){
     if(estadolicuadora == "apagada"){
         estadolicuadora = "encendido";
+        controlarSonido();
         licuadora.classList.add("active");
     }else {
         estadolicuadora = "apagada";
+        controlarSonido();
         licuadora.classList.remove("active");
+    }
+}
+
+function controlarSonido(){
+    if(sonidoLicuadora.paused){
+        botonLicuadora.onplay();
+        sonidoLicuadora.onplay();
+    } else {
+        botonLicuadora.onplay();
+        sonidoLicuadora.onpause();
+        sonidoLicuadora.currentTime = 0;
     }
 }
